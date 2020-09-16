637847232fa4d264ed55cab84fd146a16a93909e
w40141 <w.40141@gmail.com>
2020-09-16 16:29:24 +0900

    add error_handler and edit delete_job

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from models.models import AllJobs
+from flask import Flask, jsonify, abort
+from models.models import Job
 from models.database import db_session
 import datetime
 import hashlib
@@ -9,55 +9,86 @@ from typing import Any
 
 app = Flask(__name__)
 
+waiting = "Waiting"
+done = "Done"
+running = "Running"
+error = "Error"
+
 
 @app.route("/", methods=["GET"])
 def hello():
+    """
+    Server operation check
+    """
     return "I'm working!"
 
 
 @app.route("/sampler/<string:sampler_name>", methods=["POST"])
 def post_sample(sampler_name=None) -> Any:
     """
-    -d matrix
-    return {'job_id': 'NAME-yyyymmddHHMMSSxxxxxxxx'}
+    Post matrix to sample
     """
     job_id = make_id(sampler_name)
-    job_id = job_id
-    sampler = sampler_name
-    state = "Waiting"
-    alljobs = AllJobs(job_id, sampler, state, datetime.datetime.now())
-    db_session.add(alljobs)
+    state = waiting
+    job = Job(job_id, sampler_name, state, datetime.datetime.now())
+    db_session.add(job)
     db_session.commit()
     return jsonify({"job_id": job_id})
 
 
 def make_id(sampler_name):
+    """
+    Make job id
+    """
     dt_now = datetime.datetime.now()
-    job_id = sampler_name + "-" + dt_now.strftime("%Y%m%d%H%M%S")
-    job_id += hashlib.md5(job_id.encode()).hexdigest()[:4]
-    return job_id
+    job_id = sampler_name + "-" + dt_now.strftime("%Y%m%d%H%M%S%f%f")
+    return hashlib.md5(job_id.encode()).hexdigest()
 
 
 @app.route("/jobs/result/<string:job_id>", methods=["GET"])
 def get_result(job_id=None):
-    job = AllJobs.query.filter_by(job_id=job_id).first()
+    """
+    Get result
+    """
+    job = Job.query.filter_by(job_id=job_id).first()
+    if not job:
+        abort(404, {'code': 'Not found', 'message': 'jobid not found'})
     return jsonify(job.to_dict())
 
 
-@app.route("/jobs/result/<string:job_id>", methods=["DELETE"])
-def delete_result(job_id=None):
-    pass
+@app.route("/jobs/<string:job_id>", methods=["DELETE"])
+def delete_job(job_id=None):
+    job = Job.query.filter_by(job_id=job_id).first()
+    if not job:
+        abort(404, {'code': 'Not found', 'message': 'jobid not found'})
+
+    db_session.delete(job)
+    db_session.commit()
+    return jsonify(job.to_dict())
 
 
 @app.route("/jobs", methods=["GET"])
 def get_jobs():
-    return jsonify({'users': [job.to_dict() for job in AllJobs.query.all()]})
+    """
+    Get all registered jobs.
+    """
+    return jsonify({'users': [job.to_dict() for job in Job.query.all()]})
 
 
-@app.route("/jobs", methods=["POST"])
-def cansel_job(job_id=None):
+@app.route("/jobs/cancel", methods=["POST"])
+def cancel_job(job_id=None):
+    """
+    Cancel a job in the "Waiting" state.
+    """
     pass
 
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+@app.errorhandler(400)
+@app.errorhandler(404)
+def error_handler(error):
+    # error.code: HTTPステータスコード
+    # error.description: abortで設定したdict型
+    return jsonify({'error': {
+        'code': error.description['code'],
+        'message': error.description['message']
+    }}), error.code
